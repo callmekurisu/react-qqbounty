72956d912cd1686793df6b3be40a1948fc9c12e2
Calvin Vo <cvogit@gmail.com>
2018-11-27 21:51:24 -0600

    Replaced react-toolbox with material-ui

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
+import { Provider } from 'react-redux';
+import { store } from './Redux/Store';
+
+import Modal from './Components/Modal';
+
 import './Css/App.css';
 
 class App extends Component {
   render() {
     return (
       <Provider store={store}>
-      
+        <Modal />
       </Provider>
     );
   }
