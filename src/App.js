73277ba3b8aa7c01a5c498a1ff17a79d063394de
Calvin Vo <cvogit@gmail.com>
2018-11-27 22:06:40 -0600

    Added serviceWorker and fixed merge errors

--- src/App.js
+++ src/App.js
@@ -2,12 +2,11 @@ import React, { Component } from 'react';
 import { Provider } from 'react-redux';
 import { store } from './Redux/Store';
 
+import { LogInComponent } from './Components/Login.component';
 import Modal from './Components/Modal';
 
 import './Css/App.css';
-import { LogInComponent } from './Components/Login.component';
-import {Provider} from 'react-redux';
-import {store} from './Redux/Store'
+
 
 class App extends Component {
   render() {
