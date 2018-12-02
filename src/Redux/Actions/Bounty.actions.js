576fc704a64d2695adc73737502e8c6b7327f746
Fishai <kyleaubreyford@gmail.com>
2018-12-02 17:24:47 -0600

    cry

--- src/Redux/Actions/Bounty.actions.js
+++ src/Redux/Actions/Bounty.actions.js
@@ -1,126 +1,130 @@
 import { BountiesClient } from '../../AxiosClients/qqBountyClient';
 import axios from 'axios';
-import { userTypes }      from './User.actions';
-import { snackbarTypes }  from './Snackbar.actions';
+import { userTypes } from './User.actions';
+import { snackbarTypes } from './Snackbar.actions';
 import { SubjectSubscriber } from 'rxjs/internal/Subject';
 
 const SERVER_ADDRESS = process.env.REACT_APP_SERVER_ADDRESS;
 
 export const bountyTypes = {
-  GET_SEARCH_BOUNTIES:      'GET_SEARCH_BOUNTIES',
-  GET_NEW_BOUNTIES:         'GET_NEW_BOUNTIES',
-  GET_OLD_BOUNTIES:         'GET_OLD_BOUNTIES',
-  GET_POPULAR_BOUNTIES:     'GET_POPULAR_BOUNTIES',
-  GET_HIGH_PAY_BOUNTIES:    'GET_HIGH_PAY_BOUNTIES',
-  GET_USER_BOUNTIES:        'GET_USER_BOUNTIES',
-  POST_BOUNTY:        'POST_BOUNTY',
-  OPEN_BOUNTY_MODAL:  'OPEN_BOUNTY_MODAL',
-  CLOSE_BOUNTY_MODAL: 'CLOSE_BOUNTY_MODAL'
+  GET_SEARCH_BOUNTIES: 'GET_SEARCH_BOUNTIES',
+  GET_NEW_BOUNTIES: 'GET_NEW_BOUNTIES',
+  GET_OLD_BOUNTIES: 'GET_OLD_BOUNTIES',
+  GET_POPULAR_BOUNTIES: 'GET_POPULAR_BOUNTIES',
+  GET_HIGH_PAY_BOUNTIES: 'GET_HIGH_PAY_BOUNTIES',
+  GET_USER_BOUNTIES: 'GET_USER_BOUNTIES',
+  POST_BOUNTY: 'POST_BOUNTY',
+  OPEN_BOUNTY_MODAL: 'OPEN_BOUNTY_MODAL',
+  CLOSE_BOUNTY_MODAL: 'CLOSE_BOUNTY_MODAL',
+  ADD_TO_NEW_BOUNTIES: 'ADD_TO_NEW_BOUNTIES'
 }
 
 
 
 export const getNewBounties = () => (dispatch) => {
-  axios.get(SERVER_ADDRESS+'/bounties/newest')
-  .then(response => {
-    dispatch({
-      type: bountyTypes.GET_NEW_BOUNTIES,
-      payload: {
-        newBounties: response.data.result
-      }
+
+  axios.get(SERVER_ADDRESS + '/bounties/newest')
+    .then(response => {
+      dispatch({
+        type: bountyTypes.GET_NEW_BOUNTIES,
+        payload: {
+          newBounties: response.data.result
+        }
+      });
+    })
+    .catch(error => {
     });
-  })
-  .catch(error => {
-  });
 }
 
 export const getOldBounties = () => (dispatch) => {
-  axios.get(SERVER_ADDRESS+'/bounties/oldest')
-  .then(response => {
-    dispatch({
-      type: bountyTypes.GET_OLD_BOUNTIES,
-      payload: {
-        oldBounties: response.data.result
-      }
+  axios.get(SERVER_ADDRESS + '/bounties/oldest')
+    .then(response => {
+      dispatch({
+        type: bountyTypes.GET_OLD_BOUNTIES,
+        payload: {
+          oldBounties: response.data.result
+        }
+      });
+    })
+    .catch(error => {
     });
-  })
-  .catch(error => {
-  });
 }
 
 export const getPopularBounties = () => (dispatch) => {
-  axios.get(SERVER_ADDRESS+'/bounties/popular')
-  .then(response => {
-    dispatch({
-      type: bountyTypes.GET_POPULAR_BOUNTIES,
-      payload: {
-        popularBounties: response.data.result
-      }
+  axios.get(SERVER_ADDRESS + '/bounties/popular')
+    .then(response => {
+      dispatch({
+        type: bountyTypes.GET_POPULAR_BOUNTIES,
+        payload: {
+          popularBounties: response.data.result
+        }
+      });
+    })
+    .catch(error => {
+
     });
-  })
-  .catch(error => {
- 
-  });
 }
 
 export const getHighPayBounties = () => (dispatch) => {
-  axios.get(SERVER_ADDRESS+'/bounties/cost')
-  .then(response => {
-    dispatch({
-      type: bountyTypes.GET_HIGH_PAY_BOUNTIES,
-      payload: {
-        highPayBounties: response.data.result
-      }
-    });
-  })
-  .catch(error => {
+  axios.get(SERVER_ADDRESS + '/bounties/cost')
+    .then(response => {
+      dispatch({
+        type: bountyTypes.GET_HIGH_PAY_BOUNTIES,
+        payload: {
+          highPayBounties: response.data.result
+        }
+      });
+    })
+    .catch(error => {
 
-  });
+    });
 }
 
 export const getUserBounties = () => (dispatch) => {
-  if(localStorage.getItem('JWT')) {
+  if (localStorage.getItem('JWT')) {
     let jwtToken = localStorage.getItem('JWT');
     axios.get(`${SERVER_ADDRESS}/bounties/user`,
-    { headers: {
-       'Authorization': `Bearer ${jwtToken}`
-    }
-    })
-    .then((response) => { 
-      dispatch({
-        type: bountyTypes.GET_USER_BOUNTIES,
+      {
+        headers: {
+          'Authorization': `Bearer ${jwtToken}`
+        }
+      })
+      .then((response) => {
+        dispatch({
+          type: bountyTypes.GET_USER_BOUNTIES,
           payload: {
             userBounties: response.data.result
           }
+        })
       })
-    })
   }
 }
 
 export const getSearchBounties = (pSubjects) => (dispatch) => {
   let paramString = "";
-  if(pSubjects.length !== 0) {
-      paramString = "?"
-      pSubjects.map((subject)=>{
-          paramString = "subjects="+subject+"&"
-      })
-       paramString = paramString.substring(0,paramString.length-1);
+  if (pSubjects.length !== 0) {
+    paramString = "?"
+    pSubjects.map((subject) => {
+      paramString = "subjects=" + subject + "&"
+    })
+    paramString = paramString.substring(0, paramString.length - 1);
   }
-  if(localStorage.getItem('JWT')) {
+  if (localStorage.getItem('JWT')) {
     let jwtToken = localStorage.getItem('JWT');
     axios.get(`${SERVER_ADDRESS}+/bounties/subjects${paramString}`,
-      { headers: {
-        'Authorization': `Bearer ${jwtToken}`
-      }
-    })
-    .then((response) => { 
-      dispatch({
-        type: bountyTypes.GET_SEARCH_BOUNTIES,
+      {
+        headers: {
+          'Authorization': `Bearer ${jwtToken}`
+        }
+      })
+      .then((response) => {
+        dispatch({
+          type: bountyTypes.GET_SEARCH_BOUNTIES,
           payload: {
             searchBounties: response.data.result
           }
+        })
       })
-    })
   }
 }
 
@@ -128,46 +132,67 @@ export const getSearchBounties = (pSubjects) => (dispatch) => {
 
 export const submitBounty = (state) => (dispatch) => {
   console.log(state);
-  if(localStorage.getItem('JWT')) {
+  if (localStorage.getItem('JWT')) {
     let jwtToken = localStorage.getItem('JWT');
     axios.defaults.headers.common['Authorization'] = 'Bearer ' + jwtToken;
-    axios.post(SERVER_ADDRESS+'/bounties',state)
-    .then(response => {
-      dispatch({
-        type: userTypes.SET_USER_INFO,
-        payload: {
-          user: response.data.result.user
-        }
+    axios.post(SERVER_ADDRESS + '/bounties', state)
+      .then(response => {
+
+        // setTimeout(()=>{
+        // axios.get(SERVER_ADDRESS + '/bounties/newest')
+        //   .then(response => {
+        //     dispatch({
+        //       type: bountyTypes.GET_NEW_BOUNTIES,
+        //       payload: {
+        //         newBounties: response.data.result
+        //       }
+        //     });
+        //   })
+        //   .catch(error => {
+        //   });
+
+        // axios.get(SERVER_ADDRESS + '/bounties/cost')
+        //   .then(response => {
+        //     dispatch({
+        //       type: bountyTypes.GET_NEW_BOUNTIES,
+        //       payload: {
+        //         newBounties: response.data.result
+        //       }
+        //     });
+        //   })
+        //   .catch(error => {
+        //   });
+        // },2000)
+        dispatch({
+          type: snackbarTypes.SNACKBAR_ADD,
+          payload: {
+            message: "Bounty Submitted"
+          }
+        });
+
+      })
+      .catch(error => {
+
       });
-      dispatch({
-        type: snackbarTypes.SNACKBAR_ADD,
-        payload: {
-          message: "Bounty Submitted"
-        }
-      });
-    })
-    .catch(error => {
-      
-    });
   }
 }
 
 export const openBountyModal = (pBounty) => (dispatch) => {
   dispatch({
     type: bountyTypes.OPEN_BOUNTY_MODAL,
-      payload: {
-        modalOpen:      true,
-        modalBountyId:  pBounty.bountyId,
-        modalBounty:    pBounty
-      }
+    payload: {
+      modalOpen: true,
+      modalBountyId: pBounty.bountyId,
+      modalBounty: pBounty
+    }
   });
 }
 
 export const closeBountyModal = () => (dispatch) => {
   dispatch({
     type: bountyTypes.CLOSE_BOUNTY_MODAL,
-      payload: {
-        modalOpen:      false
-      }
+    payload: {
+      modalOpen: false
+    }
   });
 }
