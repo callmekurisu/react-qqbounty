9c0889aee1b0e7b0b77ded642169eb1f9e0ad96b
Calvin Vo <cvogit@gmail.com>
2018-12-02 22:58:51 -0600

    Cleaned files

--- src/Components/Navbar/Login.component.js
+++ src/Components/Navbar/Login.component.js
@@ -6,9 +6,7 @@ import InputAdornment from '@material-ui/core/InputAdornment';
 import Visibility from '@material-ui/icons/Visibility';
 import VisibilityOff from '@material-ui/icons/VisibilityOff';
 import Dialog from '@material-ui/core/Dialog';
-import DialogActions from '@material-ui/core/DialogActions';
 import DialogContent from '@material-ui/core/DialogContent';
-import DialogContentText from '@material-ui/core/DialogContentText';
 import DialogTitle from '@material-ui/core/DialogTitle';
 import { connect } from 'react-redux';
 
